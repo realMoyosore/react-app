548d5f460900f1053dd0c584507bc9a8ec633e62
Moyosore Sulola <moyixxoreoluwa@gmail.com>
2025-07-17 16:43:42 +0100

    added list

--- src/App.js
+++ src/App.js
@@ -3,6 +3,38 @@ import Greeting from "./components/greetings";
 import UserProfileCard from "./components/userProfileCard";
 import ListEx from "./components/list";
 
+const cars = ["BMW", "BENZ", "AUDI", "TOYOTA", "TESLA"];
+const carList = cars.map((item) => {
+  return <li key={item}>{item}</li>;
+});
+
+const userProfile = [
+  {
+    name: "John Doe",
+    age: 30,
+    location: "New York"
+  },
+  {
+    name: "John Doe",
+    age: 30,
+    location: "New York"
+  },
+  {
+    name: "John Doe",
+    age: 30,
+    location: "New York"
+  }
+]
+const listOfUsers = userProfile.map((user) => {
+  return (
+    <div>
+      <h2>{user.name}</h2>
+      <p>{user.age}</p>
+      <p>{user.location}</p>
+    </div>
+  );
+})
+
 const App = () => {
   //arrow function component
   return (
@@ -12,6 +44,12 @@ const App = () => {
       <ol>
         <ListEx />
       </ol>
+      <ol>
+        {carList}
+      </ol>
+      <div>
+        {listOfUsers}
+      </div>
     </section>
   );
 };
