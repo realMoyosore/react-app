7709a8197243a87e49f5d03ccbdff019272b0b04
Moyosore Sulola <moyixxoreoluwa@gmail.com>
2025-07-17 16:21:33 +0100

    added component style

--- src/components/avatar.js
+++ src/components/avatar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "../assets/styles/avatar.css";
 
 const Avatar = () => {
   return <img className="card-img" src="https://picsum.photos/id/1/200/300" alt="Profile" />;
