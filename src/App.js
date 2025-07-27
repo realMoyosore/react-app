2847ee5845da2caff38a3b339190363d7ba620d6
Moyosore Sulola <moyixxoreoluwa@gmail.com>
2025-07-27 22:34:40 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ const App = () => {
   const [count, setCount] = useState(0);
 
   return (
-  <section className="main-page">
-    <h1>Count: {count}</h1>
-    <button onClick={() => setCount(count + 1)}>Increment</button>
+    <section className="main-page">
+      <h1>Count: {count}</h1>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+      <button onClick={() => setCount(0)}>Reset</button>
+      <p>Click the buttons to change the count.</p>
+      <p>Current count is: {count}</p>
     </section>
   );
-}
+};
 
 export default App;
