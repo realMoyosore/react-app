8b24620dbbb07c19f3c6410205d5d4e656e31f1b
Moyosore Sulola <moyixxoreoluwa@gmail.com>
2025-10-25 20:56:05 -1200

    updated

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 // import { useState } from 'react';
 
-// const CounterButtons = ({ count, setCount }) => {
+const CounterButtons = ({ count, setCount }) => {
 
-//   const increment = () => {
-//     setCount(count + 1);
-//   }
+  const increment = () => {
+    setCount(count + 1);
+  }
 
-//   const decrement = () => {
-//     setCount(count - 1);
-//   }
+  const decrement = () => {
+    setCount(count - 1);
+  }
 
 //   // Conditions to display different messages based on count
 
