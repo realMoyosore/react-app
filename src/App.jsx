bb8d2006e6323eca17e2756dd6b5f0887d402bf3
Moyosore Sulola <moyixxoreoluwa@gmail.com>
2025-11-04 22:06:26 -1200

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -45,7 +45,19 @@ function UserBio() {
     });
   };
 
-
+  return (
+    <div>
+      <h1>User Bio</h1>
+      <div>{user.bio}</div>
+      <form onSubmit={handleBioUpdate}>
+        <textarea
+          value={user.bio}
+          onChange={(e) => setUser({ ...user, bio: e.target.value })}
+        />
+        <button type="submit">Update Bio</button>
+      </form>
+    </div>
+  );
 }
 
 //User profile component
